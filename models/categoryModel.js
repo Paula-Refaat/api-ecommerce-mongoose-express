620215b6b43507127221d810e5549e656f764754
Paula <poularefaat86@gmail.com>
2023-08-17 23:59:41 +0300

    Category Schema and model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 // 1- Create Schema
-const categorySchema = new mongoose.Schema({
-  name: String,
-});
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: string,
+      required: [true, "Category required"],
+      unique: [true, "Category must be unique"],
+      minLength: [3, "Too short category name"],
+      maxLength: [32, "Too long category name"],
+    },
+    //A and B => shopping.com/a-and-b
+    slug: {
+      type: string,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
 
 // 2- Create Model
 const CategoryModel = mongoose.model("Category", categorySchema);
