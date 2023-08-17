d13e2c390c5a83ee83e5de3c69de1d803f8d37d4
Paula <poularefaat86@gmail.com>
2023-08-18 02:33:24 +0300

    get categories

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,14 +1,19 @@
 const CategoryModel = require("../models/categoryModel");
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
+const { describe } = require("node:test");
 
-exports.getCategories = (req, res) => {
-  //  const name = req.body.name;
-  //  console.log(req.body);
+// @desc    Get list of category
+// @route   GET /api/categories
+// @access  Public
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await CategoryModel.find({});
+  res.status(200).json({ results: categories.length, data: categories });
+});
 
-  res.send();
-};
-
+// @desc    Create category
+// @route   POST /api/categories
+// @access  Private
 exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
   console.log(req.body);
