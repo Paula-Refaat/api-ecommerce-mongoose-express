dfa873340a03b34cc515e0bdb43b0cd710a72666
Paula <poularefaat86@gmail.com>
2023-08-17 22:35:26 +0300

    prepare categories folders

---
+++ models/categoryModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// 1- Create Schema
+const categorySchema = new mongoose.Schema({
+  name: String,
+});
+
+// 2- Create Model
+const CategoryModel = mongoose.model("Category", categorySchema);
+
+module.exports = CategoryModel;
---
+++ routes/categoryRoute.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const { getCategories } = require('../services/categoryService');
+
+
+router.get('/', getCategories); 
+
+module.exports = router;
